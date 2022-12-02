0fcdad8c4f63032a9afa957d1dfa8f1819f53174
Bright Emeka <brightc.emeka@gmail.com>
2022-12-02 14:41:04 +0100

    a few grammar fixes

--- ex1-2.py
+++ ex1-2.py
@@ -1,18 +1,17 @@
-#How many seconds are there in 42 minutes 42 secons?
+# How many seconds are there in 42 minutes 42 secons?
 secconds = ((42 * 60) + 42) * 60
-print("There are " + str(secconds) + " seconds in " + "42 minutes 42 seconds" )
+print("There are " + str(secconds) + " seconds in " + "42 minutes 42 seconds")
 
-
-#miles in 10KM, if 1M = 1.61km
+# miles in 10KM, if 1M = 1.61km
 xm = 10 / 1.61
-print("There are " + str(xm) + " miles in 10KM" )
+print("There are " + str(xm) + " miles in 10KM")
 
-#If you run a 10Km race in 42minutes 42seconds, 
+# If you run a 10Km race in 42minutes 42seconds,
 # what is your average pace (time per mile in minutes and seconds)
-#What is your average speed in miles per hour?
+# What is your average speed in miles per hour?
 
 hour = secconds / 3600
-#Find AvgPace & AvgSpeed
+# Find AvgPace & AvgSpeed
 AvgPace = secconds / xm
 AvgSpeed = xm / hour
 print("Your Average Pace is " + str(AvgPace))
